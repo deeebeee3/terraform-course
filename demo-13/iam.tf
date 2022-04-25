f9fe06bfdd79d4a312d64bc2f97a77ec80a9cc26
Deepak Bhari <deeebeee3@hotmail.com>
2022-04-25 12:41:24 +0100

    Demo-14 IAM Users and Groups

--- demo-13/iam.tf
+++ demo-13/iam.tf
@@ -1,15 +1,19 @@
 # group definition
+
+# Create the AWS IAM group (this group doesn't have access to anything yet)
 resource "aws_iam_group" "administrators" {
   name = "administrators"
 }
 
+#Attach the AWS managed Administrator policy to the group
+# (These poilicies are already created by AWS)
 resource "aws_iam_policy_attachment" "administrators-attach" {
   name       = "administrators-attach"
   groups     = [aws_iam_group.administrators.name]
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
-# user
+# Create the users
 resource "aws_iam_user" "admin1" {
   name = "admin1"
 }
@@ -18,6 +22,7 @@ resource "aws_iam_user" "admin2" {
   name = "admin2"
 }
 
+# Add the users to the group
 resource "aws_iam_group_membership" "administrators-users" {
   name = "administrators-users"
   users = [
@@ -30,4 +35,7 @@ resource "aws_iam_group_membership" "administrators-users" {
 output "warning" {
   value = "WARNING: make sure you're not using the AdministratorAccess policy for other users/groups/roles. If this is the case, don't run terraform destroy, but manually unlink the created resources"
 }
+
+# Assign passwords to the users from the AWS console or the AWS CLI.
+# We don't do if from here otherwise passwords would be in state file - not secure.
 
